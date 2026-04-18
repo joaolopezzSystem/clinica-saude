afc17ad0a6a5c8b118703920cc0b0f06f47db892
João Guilherme Lopes de Andrade <joaolopezz@gmail.com>
2026-04-18 16:03:10 -0300

    adicionando backend para deploy

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const cors = require('cors');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
